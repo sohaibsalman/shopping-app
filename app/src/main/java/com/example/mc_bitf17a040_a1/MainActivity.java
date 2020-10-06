282ee4b9a6400136ecef91754c9d8333070a564e
Sohaib Salman <sohaibsalman10@gmail.com>
2020-10-06 16:52:58 +0500

    initial commit

---
+++ app/src/main/java/com/example/mc_bitf17a040_a1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mc_bitf17a040_a1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnNext) {
+            Intent newActivity = new Intent(MainActivity.this, HomeActivity.class);
+
+            startActivity(newActivity);
+        }
+    }
+}
