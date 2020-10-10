66bc1c0c8957e4f074c5f83e0c78e00e0e11484c
Sohaib Salman <sohaibsalman10@gmail.com>
2020-10-10 20:57:01 +0500

    Change text view color on focus in personal details activity

--- app/src/main/java/com/example/mc_bitf17a040_a1/PersonalDetailsActivity.java
+++ app/src/main/java/com/example/mc_bitf17a040_a1/PersonalDetailsActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.mc_bitf17a040_a1.classes.PersonalDetails;
 
-public class PersonalDetailsActivity extends AppCompatActivity implements View.OnClickListener {
+public class PersonalDetailsActivity extends AppCompatActivity implements View.OnClickListener, View.OnFocusChangeListener{
 
     private Button btnNext;
     private Button btnPrev;
@@ -33,6 +34,16 @@ public class PersonalDetailsActivity extends AppCompatActivity implements View.O
         txtLastName = (EditText) findViewById(R.id.txtLastName);
         txtContact = (EditText) findViewById(R.id.txtContact);
         txtEmail = (EditText) findViewById(R.id.txtEmail);
+
+        txtFirstName.setOnClickListener(this);
+        txtLastName.setOnClickListener(this);
+        txtContact.setOnClickListener(this);
+        txtEmail.setOnClickListener(this);
+
+        txtFirstName.setOnFocusChangeListener(this);
+        txtLastName.setOnFocusChangeListener(this);
+        txtEmail.setOnFocusChangeListener(this);
+        txtContact.setOnFocusChangeListener(this);
 
         btnNext.setOnClickListener(this);
         btnPrev.setOnClickListener(this);
@@ -58,6 +69,54 @@ public class PersonalDetailsActivity extends AppCompatActivity implements View.O
         else if(v.getId() == R.id.btnPersonalPrev)
         {
             finish();
+        }
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if(v.getId() == R.id.txtFirstName)
+        {
+            if(hasFocus)
+            {
+                txtFirstName.setBackgroundResource(R.drawable.texbox_border_blue);
+            }
+            else
+            {
+                txtFirstName.setBackgroundResource(R.drawable.texbox_border);
+            }
+        }
+        else if(v.getId() == R.id.txtLastName)
+        {
+            if(hasFocus)
+            {
+                txtLastName.setBackgroundResource(R.drawable.texbox_border_blue);
+            }
+            else
+            {
+                txtLastName.setBackgroundResource(R.drawable.texbox_border);
+            }
+        }
+        else if(v.getId() == R.id.txtEmail)
+        {
+            if(hasFocus)
+            {
+                txtEmail.setBackgroundResource(R.drawable.texbox_border_blue);
+            }
+            else
+            {
+                txtEmail.setBackgroundResource(R.drawable.texbox_border);
+            }
+        }
+        else if(v.getId() == R.id.txtContact)
+        {
+            if(hasFocus)
+            {
+                txtContact.setBackgroundResource(R.drawable.texbox_border_blue);
+            }
+            else
+            {
+                txtContact.setBackgroundResource(R.drawable.texbox_border);
+            }
         }
     }
 }
